e979016fa9da05799314e83a0d2e4c518875d5c8
JsonPty <102558481+JasonPTY@users.noreply.github.com>
2025-07-16 19:33:32 -0500

    NEW UPDATE

--- index.php
+++ index.php
@@ -781,7 +781,7 @@ if (isset($_SESSION['usuario_id'])) {
           </section>
         </section>
         <section id="productos" class="spa-section">
-          <?php include './public/assets/productos.php'; ?>
+          <?php include './public/assets/productos_nuevo.php'; ?>
         </section>
         <section id="contacto" class="spa-section">
         <?php
