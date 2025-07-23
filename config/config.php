2eaa66fc37fec00b7a1ad72028fd29a231755613
Ian Arauz <ianarauz62@gmail.com>
2025-07-23 16:44:49 -0500

    Pequeños cambios en local en mi rama

--- config/config.php
+++ config/config.php
@@ -1,23 +1,24 @@
 <?php
 
-/*Configuración de Jason PTY*/
+/*Configuración de Jason PTY
 define("urlsite", "http://localhost/SoftwareIX/");
 
 
 $host = "localhost";
 $username = "jasonpty";
 $password = "jason27278";
+$database = "db_zonautp";*/
+
+/* Configuración de Freddy*/
+define("urlsite", "http://localhost:3000/SoftwareIX/");
+$host = "127.0.0.1";
+$username = "freddy";
+$password = "12345root";
 $database = "db_zonautp";
+
 
 $conn = new mysqli($host, $username, $password, $database);
 if ($conn->connect_error) {
     die('Error de conexión a la base de datos: ' . $conn->connect_error);
 }
-
-/* Configuración de Freddy
-define("urlsite", "http://localhost:3000/SoftwareIX/");
-$host = "127.0.0.1";
-$username = "freddy";
-$password = "12345root";
-$database = "db_zonautp";*/
 ?>
